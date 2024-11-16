a6b4372e2b01d56cdcb2db0e410c0aaa631be68b
ChristianTo <christian.to@outlook.com>
2024-11-16 14:15:22 -0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="page">
+        <div className="page__content"></div>
+        <p>HELLLO</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
