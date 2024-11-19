42ecacac578df1d5c8a65d48702c10655246673a
ChristianTo <christian.to@outlook.com>
2024-11-19 14:41:26 -0600

    upadted css for cloth and shelter

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-import { useState } from "react";
 import Header from "../src/components/Header";
-
 import Warmth from "../src/components/Warmth.jsx";
 import Shelter from "../src/components/Shelter.jsx";
 import Footer from "../src/components/Footer.jsx";
@@ -9,12 +7,12 @@ import "./App.css";
 
 function App() {
   return (
-    <>
+    <div className="app">
       <Header />
       <Shelter />
       <Warmth />
       <Footer />
-    </>
+    </div>
   );
 }
 
