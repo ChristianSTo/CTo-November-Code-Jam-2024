e4a20f978bf4cee803cd9458e789fd03b9e724be
Luci Brambila <lucerobrambilaguzman@icloud.com>
2024-11-19 22:39:15 -0600

    add links to websites

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,25 +5,24 @@ function About() {
   return (
     <>
       <section className="about">
-        <h2 className="about__title">Helping hands</h2>
-        <p className="about__info">
-          At Helping Hands, we believe in the power of community and the warmth
-          that comes from helping one another. We are a non-profit organization
-          dedicated to connecting families and individuals with the resources
-          they need, all over the country. Whether it's finding food, a warm
-          meal, or housing support, we work closely with churches, community
-          centers, and other local organizations to lend a helping hand. This
-          Thanksgiving week, we are especially focused on making sure every
-          family has access to the support and resources they need. Our goal is
-          simple: to give back and bring our community closer together by
-          ensuring no one goes without the essentials during this special time
-          of year. Together, we can make a difference.
-        </p>
-        <img
-          className="about__image"
-          src={aboutImage}
-          alt={"Image of ${aboutImage}"}
-        ></img>
+        <div className="about__container">
+          <h2 className="about__title">Helping hands</h2>
+
+          <p className="about__info">
+            At Helping Hands, we believe in the power of community and the
+            warmth that comes from helping one another. We are a non-profit
+            organization dedicated to connecting families and individuals with
+            the resources they need, all over the country. Whether it's finding
+            food, a warm meal, or housing support, we work closely with
+            churches, community centers, and other local organizations to lend a
+            helping hand. This Thanksgiving week, we are especially focused on
+            making sure every family has access to the support and resources
+            they need. Our goal is simple: to give back and bring our community
+            closer together by ensuring no one goes without the essentials
+            during this special time of year. Together, we can make a
+            difference.
+          </p>
+        </div>
       </section>
     </>
   );
--- src/components/Organization.jsx
+++ src/components/Organization.jsx
@@ -1,12 +1,71 @@
 import "../blocks/Organization.css";
+import unitedWayLogo from "../assets/unitedWayLogo.png";
+import goodWillLogo from "../assets/goodWillLogo.png";
+import salvationArmyLogo from "../assets/salvationArmyLogo.png";
+import noKidHungryLogo from "../assets/noKidHungryLogo.jpeg";
 
 function Organization() {
   return (
     <>
       <section className="organization">
-        <h2 className="organization__title">Nation Wide Organizations</h2>
-        <ul className="organization__list">
-          <li></li>
+        <h2 className="organization__title">Nationwide Organizations</h2>
+        <ul className="organization__lists">
+          <li className="organization__list-item">
+            <a
+              href="https://www.salvationarmyusa.org/usn/"
+              target="_blank"
+              className="organization__list-item_link"
+            >
+              <img
+                src={salvationArmyLogo}
+                alt="los angeles food icon"
+                className="organization__list-item_icon"
+              />
+              Salvation Army
+            </a>
+          </li>
+          <li className="organization__list-item">
+            <a
+              href="https://www.goodwill.org/"
+              target="_blank"
+              className="organization__list-item_link"
+            >
+              <img
+                src={goodWillLogo}
+                alt="arizona food icon"
+                className="organization__list-item_icon"
+              />
+              GoodWill
+            </a>
+          </li>
+          <li className="organization__list-item">
+            <a
+              href="https://www.unitedway.org/"
+              target="_blank"
+              className="organization__list-item_link"
+            >
+              <img
+                src={unitedWayLogo}
+                alt="arizona food icon"
+                className="organization__list-item_icon"
+              />
+              United Way World wide
+            </a>
+          </li>
+          <li className="organization__list-item">
+            <a
+              href="https://www.nokidhungry.org/"
+              target="_blank"
+              className="organization__list-item_link"
+            >
+              <img
+                src={noKidHungryLogo}
+                alt="los angeles food icon"
+                className="organization__list-item_icon"
+              />
+              No Kid Hungry
+            </a>
+          </li>
         </ul>
       </section>
     </>
